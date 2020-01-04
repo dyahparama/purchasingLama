21a4e0aca1879fb401beb990d918155dcd59cc59
dyah <dyah@mgesolution.com>
2020-01-04 12:14:39 +0700

    a

--- app/src/Controller/UserController.php
+++ app/src/Controller/UserController.php
@@ -1,88 +1,89 @@
 <?php
 
 use SilverStripe\CMS\Controllers\ContentController;
-use SilverStripe\Control\Controller;
 use SilverStripe\Control\Director;
 use SilverStripe\Security\Member;
 
-class UserController extends ContentController
-{
-    private static $allowed_actions = ['login', 'dologin', 'dologout', 'forgotpassword', 'test'];
+class UserController extends ContentController {
+	private static $allowed_actions = ['login', 'dologin', 'dologout', 'forgotpassword', 'test'];
 
-    public function init() {
-        parent::init();
-        @session_start();
-    }
+	public function init() {
+		parent::init();
+		@session_start();
+	}
 
-    public function index()
-    {
-        if (!self::cekSession())
-            return $this->redirect(Director::absoluteBaseURL()."user/login");
-        else
-            return $this->redirect(Director::absoluteBaseURL());
-    }
+	public function index() {
+		if (!self::cekSession()) {
+			return $this->redirect(Director::absoluteBaseURL() . "user/login");
+		} else {
+			return $this->redirect(Director::absoluteBaseURL());
+		}
 
-    public function test()
-    {
-        $email = AddOn::sendEmailSMTP('rezapahlevi056@gmail.com', 'test', array('test'), array('ForgotPassword'));
+	}
 
-        echo json_encode($email);
-    }
+	public function test() {
+		$email = AddOn::sendEmailSMTP('rezapahlevi056@gmail.com', 'test', array('test'), array('ForgotPassword'));
 
-    public function forgotpassword() {
-        if (self::cekSession())
-            return $this->redirect(Director::absoluteBaseURL());
+		echo json_encode($email);
+	}
 
-            $arr_data = array(
-                'Results' => "Zemmy",
-                'Error' => @$_SESSION['error_login']
-            );
-            return $this->customise($arr_data)
-                ->renderWith(array(
-                    'ForgotPassword'
-                ));
-    }
+	public function forgotpassword() {
+		if (self::cekSession()) {
+			return $this->redirect(Director::absoluteBaseURL());
+		}
 
-    public function login() {
-        if (self::cekSession())
-            return $this->redirect(Director::absoluteBaseURL());
+		$arr_data = array(
+			'Results' => "Zemmy",
+			'Error' => @$_SESSION['error_login'],
+		);
+		return $this->customise($arr_data)
+			->renderWith(array(
+				'ForgotPassword',
+			));
+	}
 
-        $arr_data = array(
-            'Results' => "Zemmy",
-            'Error' => @$_SESSION['error_login']
-        );
-        return $this->customise($arr_data)
-            ->renderWith(array(
-                'Login'
-            ));
-    }
+	public function login() {
+		if (self::cekSession()) {
+			return $this->redirect(Director::absoluteBaseURL());
+		}
 
-    public function dologin() {
-        $status = Auth::login($_POST);
-        if ($status) {
-            $user = Member::get()->where("`Email` = '{$_POST['email']}'")->first();
-            // var_dump($user);die;
-            // var_dump($user);
-            // die;
-            @session_unset('error_login');
-            // $_SESSION['user_nama'] = $user->Nama;
-            $_SESSION['user_id'] = $user->ID;
-            $_SESSION['user_email'] = $user->Email;
-            return $this->redirect(Director::absoluteBaseURL()."ta/");
-        }
-        $_SESSION['error_login'] = "1";
-        return $this->redirect(Director::absoluteBaseURL()."user/login");
-    }
+		$arr_data = array(
+			'Results' => "Zemmy",
+			'Error' => @$_SESSION['error_login'],
+		);
+		return $this->customise($arr_data)
+			->renderWith(array(
+				'Login',
+			));
+	}
 
-    public function dologout() {
-        session_destroy();
-        return $this->redirect(Director::absoluteBaseURL()."user/login");
-    }
+	public function dologin() {
+		$status = Auth::login($_POST);
+		if ($status) {
+			$user = Member::get()->where("`Email` = '{$_POST['email']}'")->first();
+			// var_dump($user);die;
+			// var_dump($user);
+			// die;
+			@session_unset('error_login');
+			// $_SESSION['user_nama'] = $user->Nama;
+			$_SESSION['user_id'] = $user->ID;
+			$_SESSION['user_email'] = $user->Email;
+			return $this->redirect(Director::absoluteBaseURL() . "ta/");
+		}
+		$_SESSION['error_login'] = "1";
+		return $this->redirect(Director::absoluteBaseURL() . "user/login");
+	}
 
-    static function cekSession() {
-        if (isset($_SESSION['user_id']) || !empty($_SESSION['user_id'])) {
-            return true;
-        }
-        return false;
-    }
+	public function dologout() {
+		session_destroy();
+		return $this->redirect(Director::absoluteBaseURL() . "user/login");
+	}
+
+	static function cekSession() {
+		if (isset($_SESSION['user_id']) || !empty($_SESSION['user_id'])) {
+			return true;
+		}
+		return false;
+	}
+	//test
 }
